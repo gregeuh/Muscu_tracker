41d2f0c6d68e768ded19d68aeaf586b4eb3f368e
gregeuh <gregory.troplent25@gmail.com>
2026-04-03 23:08:14 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,20 @@
-// Service Worker pour activer le mode PWA
-self.addEventListener('fetch', function(event) {});
+// On donne un nom à notre cache
+const cacheName = 'muscu-pro-v1';
+
+// On liste les ressources à garder en mémoire (optionnel mais recommandé)
+const assets = [
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=swap'
+];
+
+// Installation du Service Worker
+self.addEventListener('install', e => {
+  console.log('SW installé !');
+});
+
+// Interception des requêtes pour le mode offline
+self.addEventListener('fetch', event => {
+  // Pour l'instant, on laisse passer les requêtes normalement
+  // Mais la simple présence de ce bloc permet l'installation PWA
+});
